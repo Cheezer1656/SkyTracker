8aa954c81dece02bcda43dce57176aaaccdf18bf
Cheezer1656 <118921295+Cheezer1656@users.noreply.github.com>
2023-12-28 10:22:13 -0800

    Implement basic functionality

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,36 @@
-// place files you want to import through the `$lib` alias in this folder.
+async function get_bz_data() {
+    return await fetch("https://api.hypixel.net/v2/skyblock/bazaar").then(res => res.json());
+}
+
+function process_bz_data(data) {
+    const results = [];
+    for (const product of Object.values(data.products)) {
+        const status = product.quick_status;
+        const buy_price = status.buyPrice;
+        const sell_price = status.sellPrice;
+        const buy_demand = status.buyMovingWeek;
+        const sell_demand = status.sellMovingWeek;
+        if (buy_price === null || sell_price === null) {
+            continue;
+        } else if (buy_price > 1000 && sell_price > 1000 && buy_price / sell_price > 1.2 && buy_demand > 900000 && sell_demand > 1000000) {
+            results.push(product);
+        }
+    }
+    results.sort((x, y) => x.quick_status.buyPrice / x.quick_status.sellPrice - y.quick_status.buyPrice / y.quick_status.sellPrice);
+    results.reverse();
+    return results;
+}
+
+function simplify_num(num) {
+    if (num < 1000) {
+        return Math.round(num * 10) / 10;
+    } else if (num < 1000000) {
+        return Math.round(num / 100) / 10 + "K";
+    } else if (num < 1000000000) {
+        return Math.round(num / 100000) / 10 + "M";
+    } else {
+        return Math.round(num / 100000000) / 10 + "B";
+    }
+}
+
+export { get_bz_data, process_bz_data, simplify_num };
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+    import "src/styles.scss";
+    import { onMount } from 'svelte';
+
+    onMount(async () => {
+        await import('bootstrap/dist/js/bootstrap.js');
+    });
+</script>
+
+<nav class="navbar bg-primary">
+    <div class="container-fluid">
+        <a class="navbar-brand text-white" href="/">SkyTracker</a>
+        <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+                <a class="nav-link text-white" href="/bazaar">Bazaar</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<slot />
