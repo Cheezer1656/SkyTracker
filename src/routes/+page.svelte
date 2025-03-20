a3b1343ebb5737ed482ef890a61b3ffc040f28e3
Cheezer1656 <118921295+Cheezer1656@users.noreply.github.com>
2025-03-20 11:44:12 -0700

    Update homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,10 @@
 <svelte:head>
     <title>SkyTracker - Home</title>
 </svelte:head>
+
+<div class="container p-5">
+    <div class="text-center p-3 pb-5">
+        <h1 class="text-white"><strong>SkyTracker</strong></h1>
+        <p class="text-white">Welcome to SkyTracker! Go to the <a href="/bazaar/flips" class="text-blue">Bazaar Flips</a> page or the <a href="/ah/coflnet_crafts" class="text-blue">Coflnet Flips</a> page to get started!</p>
+    </div>
+</div>
